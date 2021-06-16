b50ad98f1c2651ceda25a587a7f70b9cf7892a61
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-06-16 17:31:42 -0400

    constants for breakout game

---
+++ backend/retro/src/main/java/com/omega/retro/games/breakout/Commons.java
@@ -0,0 +1,16 @@
+package com.omega.retro.games.breakout;
+
+public interface Commons {
+
+    int WIDTH = 300;
+    int HEIGHT = 400;
+    int BOTTOM_EDGE = 390;
+    int N_OF_BRICKS = 30;
+    int INIT_PADDLE_X = 200;
+    int INIT_PADDLE_Y = 360;
+    int INIT_BALL_X = 230;
+    int INIT_BALL_Y = 355;
+
+    // time (ms) between successive task executions that form game cycles
+    int PERIOD = 10;
+}
